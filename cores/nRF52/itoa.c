941bacc7630ef41a844854bc0bb15dc659cb489b
Sandeep Mistry <sandeep.mistry@gmail.com>
2016-03-25 09:46:21 -0400

    Put back ltoa and ultoa functions

--- cores/nRF52/itoa.c
+++ cores/nRF52/itoa.c
@@ -23,147 +23,93 @@
 extern "C" {
 #endif
 
-/* reverse:  reverse string s in place */
-/*
-static void reverse( char s[] )
+extern char* ltoa( long value, char *string, int radix )
 {
-  int i, j ;
-  char c ;
+  char tmp[33];
+  char *tp = tmp;
+  long i;
+  unsigned long v;
+  int sign;
+  char *sp;
 
-  for ( i = 0, j = strlen(s)-1 ; i < j ; i++, j-- )
+  if ( string == NULL )
   {
-    c = s[i] ;
-    s[i] = s[j] ;
-    s[j] = c ;
+    return 0 ;
   }
-}
-*/
 
-/* itoa:  convert n to characters in s */
-/*
-extern void itoa( int n, char s[] )
+  if (radix > 36 || radix <= 1)
+  {
+    return 0 ;
+  }
+
+  sign = (radix == 10 && value < 0);
+  if (sign)
+  {
+    v = -value;
+  }
+  else
+  {
+    v = (unsigned long)value;
+  }
+
+  while (v || tp == tmp)
+  {
+    i = v % radix;
+    v = v / radix;
+    if (i < 10)
+      *tp++ = i+'0';
+    else
+      *tp++ = i + 'a' - 10;
+  }
+
+  sp = string;
+
+  if (sign)
+    *sp++ = '-';
+  while (tp > tmp)
+    *sp++ = *--tp;
+  *sp = 0;
+
+  return string;
+}
+
+extern char* ultoa( unsigned long value, char *string, int radix )
 {
-  int i, sign ;
+  char tmp[33];
+  char *tp = tmp;
+  long i;
+  unsigned long v = value;
+  char *sp;
 
-  if ( (sign = n) < 0 )  // record sign
+  if ( string == NULL )
   {
-    n = -n;          // make n positive
+    return 0;
   }
 
-  i = 0;
-  do
-  {       // generate digits in reverse order
-    s[i++] = n % 10 + '0';   // get next digit
-  } while ((n /= 10) > 0) ;     // delete it
-
-  if (sign < 0 )
+  if (radix > 36 || radix <= 1)
   {
-    s[i++] = '-';
+    return 0;
   }
 
-  s[i] = '\0';
+  while (v || tp == tmp)
+  {
+    i = v % radix;
+    v = v / radix;
+    if (i < 10)
+      *tp++ = i+'0';
+    else
+      *tp++ = i + 'a' - 10;
+  }
 
-  reverse( s ) ;
+  sp = string;
+
+
+  while (tp > tmp)
+    *sp++ = *--tp;
+  *sp = 0;
+
+  return string;
 }
-*/
-
-// extern char* itoa( int value, char *string, int radix )
-// {
-//   return ltoa( value, string, radix ) ;
-// }
-
-// extern char* ltoa( long value, char *string, int radix )
-// {
-//   char tmp[33];
-//   char *tp = tmp;
-//   long i;
-//   unsigned long v;
-//   int sign;
-//   char *sp;
-
-//   if ( string == NULL )
-//   {
-//     return 0 ;
-//   }
-
-//   if (radix > 36 || radix <= 1)
-//   {
-//     return 0 ;
-//   }
-
-//   sign = (radix == 10 && value < 0);
-//   if (sign)
-//   {
-//     v = -value;
-//   }
-//   else
-//   {
-//     v = (unsigned long)value;
-//   }
-
-//   while (v || tp == tmp)
-//   {
-//     i = v % radix;
-//     v = v / radix;
-//     if (i < 10)
-//       *tp++ = i+'0';
-//     else
-//       *tp++ = i + 'a' - 10;
-//   }
-
-//   sp = string;
-
-//   if (sign)
-//     *sp++ = '-';
-//   while (tp > tmp)
-//     *sp++ = *--tp;
-//   *sp = 0;
-
-//   return string;
-// }
-
-// // extern char* utoa( unsigned long value, char *string, int radix )
-// // {
-// //   return ultoa( value, string, radix ) ;
-// // }
-
-// extern char* ultoa( unsigned long value, char *string, int radix )
-// {
-//   char tmp[33];
-//   char *tp = tmp;
-//   long i;
-//   unsigned long v = value;
-//   char *sp;
-
-//   if ( string == NULL )
-//   {
-//     return 0;
-//   }
-
-//   if (radix > 36 || radix <= 1)
-//   {
-//     return 0;
-//   }
-
-//   while (v || tp == tmp)
-//   {
-//     i = v % radix;
-//     v = v / radix;
-//     if (i < 10)
-//       *tp++ = i+'0';
-//     else
-//       *tp++ = i + 'a' - 10;
-//   }
-
-//   sp = string;
-
-
-//   while (tp > tmp)
-//     *sp++ = *--tp;
-//   *sp = 0;
-
-//   return string;
-// }
 
 #ifdef __cplusplus
 } // extern "C"
--- cores/nRF52/itoa.h
+++ cores/nRF52/itoa.h
@@ -22,11 +22,7 @@
 extern "C"{
 #endif
 
-//extern void itoa( int n, char s[] ) ;
-
-// extern char* itoa( int value, char *string, int radix ) ;
 extern char* ltoa( long value, char *string, int radix ) ;
-// extern char* utoa( unsigned long value, char *string, int radix ) ;
 extern char* ultoa( unsigned long value, char *string, int radix ) ;
 
 #ifdef __cplusplus
