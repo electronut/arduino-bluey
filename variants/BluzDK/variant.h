80f17d9e597f00da736c029585ca05002e3b9c4a
Sandeep Mistry <sandeep.mistry@gmail.com>
2016-04-26 20:33:49 -0400

    Correct NUM_DIGITAL_PINS

--- variants/BluzDK/variant.h
+++ variants/BluzDK/variant.h
@@ -33,7 +33,7 @@ extern "C"
 
 // Number of pins defined in PinDescription array
 #define PINS_COUNT           (26u)
-#define NUM_DIGITAL_PINS     (20u)
+#define NUM_DIGITAL_PINS     (22u)
 #define NUM_ANALOG_INPUTS    (6u)
 #define NUM_ANALOG_OUTPUTS   (0u)
 
--- variants/nRF52DK/variant.h
+++ variants/nRF52DK/variant.h
@@ -33,7 +33,7 @@ extern "C"
 
 // Number of pins defined in PinDescription array
 #define PINS_COUNT           (25u)
-#define NUM_DIGITAL_PINS     (14u)
+#define NUM_DIGITAL_PINS     (20u)
 #define NUM_ANALOG_INPUTS    (6u)
 #define NUM_ANALOG_OUTPUTS   (0u)
 
